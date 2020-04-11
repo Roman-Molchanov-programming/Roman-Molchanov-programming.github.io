c81a70477f43e9f05005e82839e94388c6c8f4db
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-04-11 16:20:54 +0300

    State as of 2020-04-11

---
+++ Paskal-ABC-DZ/OOO-ooo-9clas-DZ.pas
@@ -0,0 +1,22 @@
+﻿var St: string;
+n, k1, k2: integer;
+begin
+writeln('Введите текст');
+readln(St);
+n := length(St);
+k1 := 0; k2 := 0;
+for var i := 1 to n do
+begin
+if St[i] = 'о' then
+k1 := k1 + 1;
+if St[i] = 'О' then
+k2 := k2 + 1;
+end;
+if k1 > k2 then
+writeln('Маленьких (-о) больше')
+else
+if k2 > k1 then
+writeln('Больших (-О) больше')
+else
+writeln('Количество больших (-О) равно количеству маленьких (-о)');
+end.
