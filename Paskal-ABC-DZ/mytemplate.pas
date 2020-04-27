2032980992c56b12beba3c818e75e51f1a6f9b42
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-04-27 09:53:42 +0300

    Сфоткал свою клавиатуру в нормальном освещении

---
+++ Paskal-ABC-DZ/mytemplate.pas
@@ -0,0 +1,10 @@
+﻿Program mytemplate;
+Var titleAndHeading1: string;
+Begin
+  Writeln ('Введите фразу, которая будет означать <title> and <h1>.');
+  Readln (titleAndHeading1); 
+  Writeln('<!DOCTYPE html>');
+  Writeln('<html lang="ru"> ');
+
+  
+End.
