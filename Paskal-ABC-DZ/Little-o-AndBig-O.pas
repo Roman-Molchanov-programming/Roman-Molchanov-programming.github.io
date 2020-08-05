ad31dec323704e1b1a50eb6d939d1f47c9b3dade
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-08-05 12:32:39 +0300

    Конспект паскаль Простые и составные условия

---
+++ Paskal-ABC-DZ/Little-o-AndBig-O.pas
@@ -0,0 +1,22 @@
+﻿var st: string;
+n, k, r: integer;
+begin
+writeln('Введите текст');
+readln(st);
+n := length(st);
+k := 0;
+r := 0;
+for var i := 1 to n do
+begin
+if st[i] = 'о' then
+k := k + 1;
+if st[i] = 'О' then
+r := r + 1;
+end;
+if k > r then
+writeln('В тексте больше малениких - о');
+if k < r then
+writeln('В тексте больше БОЛЬШИХ - О');
+if k = r then
+writeln('В тексте и маленьких о и БОЛЬШИХ О поровну');
+end.
--- Paskal-ABC-DZ/Predlojeniya.pas
+++ Paskal-ABC-DZ/Predlojeniya.pas
@@ -6,13 +6,15 @@ readln(st);
 n := length(st);
 k := 0;
 for var i := 1 to n do
-begin
-if st[i] = '!' then
-k := k + 1;
-if st[i] = '?' then
-k := k + 1;
-if st[i] = '.' then
-k := k + 1;
+  if st[i] = '!' then
+      writeln(k);
+    k := k + 1;
+  
+  if st[i] = '?' then
+    
+    k := k + 1;
+  if st[i] = '.' then
+    k := k + 1;
 end;
-writeln('В тексте ', k, 'предложени(-е/-й)');
+writeln('В тексте ', k, ' предложени(-е/-й)');
 end.
