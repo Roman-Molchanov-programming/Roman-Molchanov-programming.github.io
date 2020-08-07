bd664b533cea97ee1e6870653116d99fffc5ebd8
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-08-07 10:07:06 +0300

    Программа которая узнает процент символа от текста

---
+++ Paskal-ABC-DZ/PercentageOfText.pas
@@ -0,0 +1,23 @@
+﻿var Text: string;
+TextPercentageSymbol, SentenceseOfText, OllSymbols, HowMuchTextPercentageSymbol, Percentage: integer;
+begin
+writeln('Введите текст');
+readln(Text);
+writeln('Введите процентный символ');
+readln(TextPercentageSymbol);
+SentenceseOfText := length(Text);
+OllSymbols := 0;
+HowMuchTextPercentageSymbol := 0;
+for var i := 1 to SentenceseOfText do
+begin
+if Text[i] = 'йцукенгшщзхъэждлорпавыфёячсмитьбюЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЁЯЧСМИТЬБЮ.,!?:"-qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890@(){}[];_' then
+OllSymbols := OllSymbols + 1;
+end;
+for var i := 1 to SentenceseOfText do
+Begin
+if (Text[i] := TextPercentageSymbol) then
+HowMuchTextPercentageSymbol := HowMuchTextPercentageSymbol + 1;
+end;
+Percentage := (HowMuchTextPercentageSymbol div OllSymbols) * 100;
+writeln('Процент ',TextPercentageSymbol,' из всего текста =', Percentage,'%');
+end.
