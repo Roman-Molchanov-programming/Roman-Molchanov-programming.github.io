45900938c8d6d417d2b5b1d330108a6fa376e214
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-08-07 15:10:19 +0300

    Поправление программы - какой процент составляет символ а их может быть несколько (например а а или б б) от текста

--- Paskal-ABC-DZ/PercentageOfText.pas
+++ Paskal-ABC-DZ/PercentageOfText.pas
@@ -1,23 +1,30 @@
-﻿var Text: string;
-TextPercentageSymbol, SentenceseOfText, OllSymbols, HowMuchTextPercentageSymbol, Percentage: integer;
+﻿var Text, TextPercentageSymbol, Oll: string;
+SentenceseOfText, OllSymbols, HowMuchTextPercentageSymbol, Percentage: integer;
 begin
-writeln('Введите текст');
-readln(Text);
-writeln('Введите процентный символ');
-readln(TextPercentageSymbol);
-SentenceseOfText := length(Text);
-OllSymbols := 0;
-HowMuchTextPercentageSymbol := 0;
-for var i := 1 to SentenceseOfText do
-begin
-if Text[i] = 'йцукенгшщзхъэждлорпавыфёячсмитьбюЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЁЯЧСМИТЬБЮ.,!?:"-qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890@(){}[];_' then
-OllSymbols := OllSymbols + 1;
-end;
-for var i := 1 to SentenceseOfText do
-Begin
-if (Text[i] := TextPercentageSymbol) then
-HowMuchTextPercentageSymbol := HowMuchTextPercentageSymbol + 1;
-end;
-Percentage := (HowMuchTextPercentageSymbol div OllSymbols) * 100;
-writeln('Процент ',TextPercentageSymbol,' из всего текста =', Percentage,'%');
+//Percentage - переменная отвечающая за процент символа в тексте
+//TextPercentageSymbol - все символы текста
+//TextPercentageSymbol - символ который вводит человек
+//HowMuchTextPercentageSymbol - Сколько раз встречается введенный выше символ
+//SentenceseOfText - используется для цикла (for var)
+//Oll - используется как словарь хранящий символы
+    writeln('Введите текст');
+    readln(Text);
+    writeln('Введите символ');
+    readln(TextPercentageSymbol);
+    SentenceseOfText := length(Text);
+    OllSymbols := 0;
+    HowMuchTextPercentageSymbol := 0;
+    Oll := 'йцукенгшщзхъэждлорпавыфёячсмитьбюЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЁЯЧСМИТЬБЮ.,!?:"-qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890@(){}[];_';
+    for var i := 1 to SentenceseOfText do
+        begin
+            if Text[i] = Oll then
+            OllSymbols := OllSymbols + 1;
+        end;
+    for var i := 1 to SentenceseOfText do
+    Begin
+        if (Text[i] = TextPercentageSymbol) then
+        HowMuchTextPercentageSymbol := HowMuchTextPercentageSymbol + 1;
+    end;
+    Percentage := (HowMuchTextPercentageSymbol div OllSymbols) * 100;
+    writeln('Процент ',TextPercentageSymbol,' из всего текста =', Percentage,'%');
 end.
