ebed724978b3339aa067e7df509fde747759835e
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-08-12 14:22:41 +0300

    Создание 2 программ по паскаль на анализ текста и его проверки

---
+++ Paskal-ABC-DZ/AnalizLattersOfWord-9class.pas
@@ -0,0 +1,39 @@
+﻿uses GraphABC;
+var world, glasnie, soglasnie, yellowLatter: string;
+lengthOfworld, analizGlasnix, analizSoglasnix, analizYellowlatter: integer;
+begin
+writeln('Введите слово');
+readln(world); writeln(world);
+lengthOfworld := length(world);
+glasnie := 'ЁУЕЭОАЫЯИЮёуеэоаыяию';
+yellowLatter := 'ьъЪЬ';
+soglasnie := 'йцкнгшщзхфвпрлджчсмтбюЙЦКНГШЩЗХФВПРЛДЖЧСМТБЮ';
+
+for var i := 1 to lengthOfworld do
+begin
+analizGlasnix := pos(world[i], glasnie);
+if analizGlasnix <> 0 then
+begin
+SetFontColor(clRed);
+end;
+end;
+
+for var i := 1 to lengthOfworld do
+begin
+analizyellowLatter := pos(world[i], yellowLatter);
+if analizyellowLatter <> 0 then
+begin
+SetFontColor(clYellow);
+end;
+end;
+
+for var i := 1 to lengthOfworld do
+begin
+analizSoglasnix := pos(world[i], soglasnie);
+if analizSoglasnix <> 0 then
+begin
+SetFontColor(clBlue);
+end;
+end;
+
+end.
---
+++ Paskal-ABC-DZ/Firstskobka-and-Endskobka-9class-dz.pas
@@ -0,0 +1,30 @@
+﻿uses GraphABC;
+var text: string;
+lengthOfText, skobkaFirst, skobkaEnd: integer;
+begin
+    writeln('Введите текст');
+    readln(text); writeln(text);
+    lengthOfText := length(text);
+    skobkaFirst := 0;
+    skobkaEnd := 0;
+    
+    for var i := 1 to lengthOfText do
+    begin
+        if text[i] = '(' then
+    begin
+        skobkaFirst := skobkaFirst + 1;
+    end;
+    
+    if text[i] = ')' then
+    begin
+        skobkaEnd := skobkaEnd + 1;
+    end;
+    end;
+    
+    if skobkaFirst = skobkaEnd then
+    writeln('Количество скобок одинаково');
+    if skobkaFirst > skobkaEnd then
+    writeln('Количество открывающих скобок больше чем закрывающих');
+    if skobkaFirst < skobkaEnd then
+    writeln('Количество закрывающих скобок больше чем открывающих');
+end.
