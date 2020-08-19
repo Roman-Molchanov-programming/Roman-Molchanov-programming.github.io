aa8ec33875956d0b20a0204eabccfa13196c9d8e
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-08-19 19:10:40 +0300

    Создание 2 программ на паскале 1 уравнени 2 четные и нечетныетцифры пишет синим и светло синим

---
+++ Paskal-ABC-DZ/ChetnieBlue-NechetnieLightblue.pas
@@ -0,0 +1,25 @@
+﻿uses GraphABC;
+var text, slovarNechet, slovarChet: string;
+//color and color1 отвечают за цвет цифры
+lengthOfText, color, color1: integer;
+begin
+    writeln('Введите числа');
+    readln(text); writeln(text);
+    lengthOfText := length(text);
+    slovarNechet := '013579';
+    slovarChet := '2468';
+    for var i := 1 to lengthOfText do
+    begin
+        color := pos(text[i], slovarNechet);
+        if color <> 0 then
+    Begin
+        SetFontColor(cllightBlue);
+    end;
+    color1 := pos(text[i], slovarChet);
+    if color1 <> 0 then
+    Begin
+        SetFontColor(clBlue);
+    end;
+        write(text[i]);
+    end;
+end.
---
+++ Paskal-ABC-DZ/Uravnenie-9class.pas
@@ -0,0 +1,20 @@
+﻿uses GraphABC;
+var text, slovarOfNamber: string;
+lengthOfText, blueNamber: integer;
+begin
+writeln('Введите уравнение');
+readln(text); writeln(text);
+lengthOfText := length(text);
+slovarOfNamber := '1234567890';
+for var i := 1 to lengthOfText do
+begin
+blueNamber := pos(text[i], slovarOfNamber);
+if blueNamber <> 0 then
+begin
+SetFontColor(clBlue);
+end
+else
+SetFontColor(clLightBlue);
+write(text[i]);
+end;
+end.
