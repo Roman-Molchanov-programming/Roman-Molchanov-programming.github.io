ff4840d5f5f30889ca54e7d2dc19f91f78884b27
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-08-20 10:50:01 +0300

    Исправление ошибок в программе на онализ букв и назначения цвета для их

--- Paskal-ABC-DZ/AnalizLattersOfWord-9class.pas
+++ Paskal-ABC-DZ/AnalizLattersOfWord-9class.pas
@@ -2,38 +2,31 @@
 var world, glasnie, soglasnie, yellowLatter: string;
 lengthOfworld, analizGlasnix, analizSoglasnix, analizYellowlatter: integer;
 begin
-writeln('Введите слово');
-readln(world); writeln(world);
-lengthOfworld := length(world);
-glasnie := 'ЁУЕЭОАЫЯИЮёуеэоаыяию';
-yellowLatter := 'ьъЪЬ';
-soglasnie := 'йцкнгшщзхфвпрлджчсмтбюЙЦКНГШЩЗХФВПРЛДЖЧСМТБЮ';
-
-for var i := 1 to lengthOfworld do
-begin
-analizGlasnix := pos(world[i], glasnie);
-if analizGlasnix <> 0 then
-begin
-SetFontColor(clRed);
-end;
-end;
-
-for var i := 1 to lengthOfworld do
-begin
-analizyellowLatter := pos(world[i], yellowLatter);
-if analizyellowLatter <> 0 then
-begin
-SetFontColor(clYellow);
-end;
-end;
-
-for var i := 1 to lengthOfworld do
-begin
-analizSoglasnix := pos(world[i], soglasnie);
-if analizSoglasnix <> 0 then
-begin
-SetFontColor(clBlue);
-end;
-end;
-
+    writeln('Введите слово');
+    readln(world); writeln(world);
+    lengthOfworld := length(world);
+    glasnie := 'ЁУЕЭОАЫЯИЮёуеэоаыяию';
+    yellowLatter := 'ьъЪЬ';
+    soglasnie := 'йцкнгшщзхфвпрлджчсмтбюЙЦКНГШЩЗХФВПРЛДЖЧСМТБЮ';
+    
+    for var i := 1 to lengthOfworld do
+    begin
+        analizGlasnix := pos(world[i], glasnie);
+        if analizGlasnix <> 0 then
+        begin
+            SetFontColor(clRed);
+        end;
+        analizyellowLatter := pos(world[i], yellowLatter);
+        if analizyellowLatter <> 0 then
+        begin
+            SetFontColor(clYellow);
+        end;
+        
+        analizSoglasnix := pos(world[i], soglasnie);
+        if analizSoglasnix <> 0 then
+        begin
+            SetFontColor(clBlue);
+        end;
+        write(world[i]);
+    end;
 end.
