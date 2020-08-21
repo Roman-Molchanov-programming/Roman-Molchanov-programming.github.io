d3b3e9f6a2ffadfbe9298d5e00eedb0e87204379
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-08-21 10:14:55 +0300

    Программа которая преобразовывает а в *

---
+++ Paskal-ABC-DZ/Apreobrazovatel-9class.pas
@@ -0,0 +1,13 @@
+﻿var text: string; lengthOfText: integer;
+begin
+    writeln('Введите строку');
+    readln(text);
+    lengthOfText := length(text);
+    for var i := 1 to lengthOfText do
+    Begin
+        if (text[i] = 'а') or (text[i] = 'a') then
+        text[i] := '*';
+    end;
+    writeln('Преобразованная строка: ');
+    writeln(text);
+end.
