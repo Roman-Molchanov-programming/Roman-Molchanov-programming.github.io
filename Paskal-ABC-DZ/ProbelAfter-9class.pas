aaa9d4f148c47f891a661adc032032277768c80b
Roman Molchanov <Molcanovroman2008@gmail.com>
2020-09-07 10:50:14 +0300

    Прошрамма которая возле знака - ставит пробелы

---
+++ Paskal-ABC-DZ/ProbelAfter-9class.pas
@@ -0,0 +1,23 @@
+﻿var text: string;
+probel: integer;
+begin
+    writeln('Введите строку');
+    readln(text);
+    probel := 2;
+    while probel < length(text) do
+    begin
+        if text[probel] = '-' then
+        begin
+            if text[probel+1] <> ' ' then
+            insert(' ',text, probel+1);
+            if text[probel-1] <> ' ' then
+            begin
+                insert(' ',text, probel);
+                probel := probel + 1;
+            end
+        end;
+        probel := probel + 1;
+    end;
+    writeln('Преобразованная строка: ');
+    writeln(text);
+end.
